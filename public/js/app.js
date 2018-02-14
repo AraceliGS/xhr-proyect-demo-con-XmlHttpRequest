96f2955432bbb7178384e9fe2f0ecb3a4a5ef957
AraceliGS <aragutarras@gmail.com>
2018-02-13 21:42:58 -0500

    añadiendo avances en la funcionalidad- obteniendo base de datos

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,27 @@ const searchField = document.getElementById('search-keyword');
 const responseContainer = document.getElementById('response-container');
 let searchedForText;
 // Agregándole al formulario el evento submit
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
   responseContainer.innerHTML = '';
   searchedForText = searchField.value;
   getNews();
 });
 
-function getNews() {}
+function addNews() {
+  const data = JSON.parse(this.responseText);
+  const response = data.response;
+  console.log(response);
+}
+
+function handleError() {
+  console.log('Se ha presentado un error');
+}
+
+function getNews() {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=f94208e7ec01409c9864a5ca6f2ba4e5`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+}
