da20a6bc2e0a99823522bdf43ff5bcaead6ece04
AraceliGS <aragutarras@gmail.com>
2018-02-13 21:04:37 -0500

    añadiendo avance en el maquetado y la funcionalidad- servidor creado

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
+// Agregándole al formulario el evento submit
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  getNews();
+});
 
+function getNews() {}
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const app = express();
+// Creando servidor
+const server = app.listen(3000, turnOn);
+function turnOn() {
+  console.log('servidor encendido');
+}
 
+app.use(express.static('public'));
