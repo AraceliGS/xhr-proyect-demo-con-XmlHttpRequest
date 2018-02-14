33f894b1ac0760babf4e5dd8b3a8702c689c5156
AraceliGS <aragutarras@gmail.com>
2018-02-13 22:33:48 -0500

    añadiendo últimos avances en la funcionalidadç

--- public/js/app.js
+++ public/js/app.js
@@ -12,8 +12,16 @@ form.addEventListener('submit', function(event) {
 
 function addNews() {
   const data = JSON.parse(this.responseText);
-  const response = data.response;
-  console.log(response);
+  console.log(data.response);
+  const article = data.response.docs[0];
+  const title = article.headline.main;
+  const snippet = article.snippet;
+
+  let li = document.createElement('li');
+  li.className = 'articleClass';
+  li.innerText = snippet;
+
+  responseContainer.appendChild(li);
 }
 
 function handleError() {
